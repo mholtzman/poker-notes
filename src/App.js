4031047789ad48dd436301fd610c92eaad9c9870
Matt Holtzman <mholtzman@schonfeld.com>
2018-04-18 22:13:20 -0400

    added react classes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,14 @@
 import React, { Component } from 'react';
-import { Card } from './cards'
-import './App.css';
 
-class CardSelector extends Component {
-    constructor(props) {
-        super(props);
-        let deck = ['h', 'c', 'd', 's'].map(suit => {
-            return [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'].reverse().map(rank => {
-                return <Card key={rank + suit} id={rank + suit}/>;
-            });
-        });
+import { PlayerSelect } from './player-select'
+import Pot from './pot'
 
-        this.state = { deck };
-    }
-
-    render() {
-        return (
-            <SelectionPanel deck={this.state.deck} />
-        )
-    }
-}
-
-class SelectionPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { numPicked: 0 };
-    }
-
+export default class App extends Component {
     render() {
         return (
             <div>
-                {this.props.deck}
-                <div>Num selected: {this.state.numPicked}</div>
-            </div>
-        );
-    }
-
-    updateSelection = (code) => {
-        console.log("UPDATING: " + code)
-        this.setState(prevState => {
-            return { deck: [{code: '6h', selected: true}] };
-        });
-    };
-}
-
-class Board extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isBlack: false, cards: []};
-    }
-
-    render() {
-        return (
-            <div onClick={this.handleClick} className={`board ${this.state.isBlack ? "black" : "white"}`}>
-                {this.state.cards}
+                <PlayerSelect />
             </div>
         )
     }
 }
-
-export default () => {
-    return (
-        <div>
-            <CardSelector />
-        </div>
-    )
-};
